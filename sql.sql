afbf0269741a7475eec583bad7391a906a580334
vishal-pandey <pandeyvishal25@yahoo.com>
2018-07-05 17:23:16 +0000

    database

---
+++ sql.sql
@@ -0,0 +1,182 @@
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: cbts
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `application`
+--
+
+DROP TABLE IF EXISTS `application`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `application` (
+  `user` varchar(20) DEFAULT NULL,
+  `tid` bigint(10) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `appdate` varchar(50) DEFAULT NULL,
+  `center` text,
+  `timing` text,
+  `rollno` varchar(12) DEFAULT NULL,
+  KEY `user` (`user`),
+  KEY `tid` (`tid`),
+  CONSTRAINT `application_ibfk_1` FOREIGN KEY (`user`) REFERENCES `student` (`mobile`),
+  CONSTRAINT `application_ibfk_2` FOREIGN KEY (`tid`) REFERENCES `test` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `application`
+--
+
+LOCK TABLES `application` WRITE;
+/*!40000 ALTER TABLE `application` DISABLE KEYS */;
+INSERT INTO `application` VALUES ('9818630411',1,'payfee','25 Jul 2017','Address 1',NULL,NULL),('9818630411',2,'started','15 Aug 2017',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `application` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `coupon100`
+--
+
+DROP TABLE IF EXISTS `coupon100`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `coupon100` (
+  `code` bigint(10) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coupon100`
+--
+
+LOCK TABLES `coupon100` WRITE;
+/*!40000 ALTER TABLE `coupon100` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coupon100` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `coupon900`
+--
+
+DROP TABLE IF EXISTS `coupon900`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `coupon900` (
+  `code` bigint(10) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coupon900`
+--
+
+LOCK TABLES `coupon900` WRITE;
+/*!40000 ALTER TABLE `coupon900` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coupon900` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `name` varchar(80) DEFAULT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `dob` varchar(20) DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`mobile`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES ('Vishal Pandey','9818630411','pandeyvishal25@yahoo.com','01/01/1998','Male');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `startdate` varchar(50) DEFAULT NULL,
+  `enddate` varchar(50) DEFAULT NULL,
+  `testcount` int(5) DEFAULT NULL,
+  `fee` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `test`
+--
+
+LOCK TABLES `test` WRITE;
+/*!40000 ALTER TABLE `test` DISABLE KEYS */;
+INSERT INTO `test` VALUES (1,'SSC Single Test','01/06/2017','01/07/2017',1,100),(2,'SSC 10 Test Series','01/06/2017','01/07/2017',10,900);
+/*!40000 ALTER TABLE `test` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `timing`
+--
+
+DROP TABLE IF EXISTS `timing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `timing` (
+  `sno` int(4) NOT NULL AUTO_INCREMENT,
+  `ddatetime` text,
+  `count` int(5) DEFAULT NULL,
+  PRIMARY KEY (`sno`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `timing`
+--
+
+LOCK TABLES `timing` WRITE;
+/*!40000 ALTER TABLE `timing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `timing` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-05 17:22:40
